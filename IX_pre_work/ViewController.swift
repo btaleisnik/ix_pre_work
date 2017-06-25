8119403734d55dd9176c1960dbaeaaed7298f30d
Brandon Taleisnik <btaleisnik@gmail.com>
2017-06-25 23:12:26 +0200

    Initial Commit

---
+++ IX_pre_work/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IX_pre_work
+//
+//  Created by Brandon Taleisnik on 6/25/17.
+//  Copyright © 2017 Brandon Taleisnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
